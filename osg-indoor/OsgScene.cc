153dc1b18e004cd02b8e04542eca9470145e20ec
yodarnitzan <34024991+yodarnitzan@users.noreply.github.com>
2017-11-27 14:22:51 +0200

    Add files via upload

---
+++ osg-indoor/OsgScene.cc
@@ -0,0 +1,58 @@
+//
+// This file is part of an OMNeT++/OMNEST simulation example.
+//
+// Copyright (C) 2015 OpenSim Ltd.
+//
+// This file is distributed WITHOUT ANY WARRANTY. See the file
+// `license' for details on this and other legal matters.
+//
+
+#ifdef WITH_OSG
+#include <osgDB/ReadFile>
+#include "OsgScene.h"
+
+using namespace omnetpp;
+
+Define_Module(OsgScene);
+
+OsgScene *OsgScene::instance = nullptr;
+
+OsgScene::OsgScene()
+{
+    if (instance)
+        throw cRuntimeError("There can be only one OsgScene instance in the network");
+    instance = this;
+}
+
+OsgScene::~OsgScene()
+{
+    instance = nullptr;
+}
+
+void OsgScene::initialize()
+{
+    scene = osgDB::readNodeFile(par("scene"));
+    if (!scene)
+        throw cRuntimeError("Could not read scene file \"%s\"", par("scene").stringValue());
+
+    cOsgCanvas *builtinOsgCanvas = getParentModule()->getOsgCanvas();
+    builtinOsgCanvas->setScene(scene);
+    builtinOsgCanvas->setGenericViewpoint(cOsgCanvas::Viewpoint(
+            cOsgCanvas::Vec3d(20, -30, 30),
+            cOsgCanvas::Vec3d(30, 20, 0),
+            cOsgCanvas::Vec3d(0, 0, 1)));
+}
+
+OsgScene *OsgScene::getInstance()
+{
+    if (!instance)
+        throw cRuntimeError("OsgScene::getInstance(): there is no OsgScene module in the network");
+    return instance;
+}
+
+void OsgScene::handleMessage(cMessage *msg)
+{
+    throw cRuntimeError("This module does not handle messages from the outside");
+}
+
+#endif  // WITH_OSG
